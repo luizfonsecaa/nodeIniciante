d010c37ee210f68bcaac05ed75e01616a865a433
luizfonsecaa <33426620+luizfonsecaa@users.noreply.github.com>
2018-09-19 17:03:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+consign().include('routes').include('utils').into(app);
+
+//qual porta estou escutando
+app.listen(3000, '127.0.0.1', ()=>{
+  console.log('servidor rodando!');
+});
+
+
---
+++ routes/users.js
@@ -0,0 +1,32 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+  filename:'users.db',
+  autoload:true
+});
+module.exports = (app) =>{
+  let route = app.route('/users');
+
+  route.get((req, res)=>{
+    db.find({}).sort({nome:1}).exec((err, users)=>{
+      if(err){
+          app.utils.error.send(err, req, res);
+      }else{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+          users
+         });
+      }
+    });
+  });
+
+  route.post((req, res)=>{
+    db.insert(req.body, (err, user)=>{
+      if(err){
+        app.utils.error.send(err, req, res);
+      }else{
+        res.status(200).json(user);
+      }
+    });
+  });
+};
